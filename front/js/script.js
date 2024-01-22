74755739ba6a0b864e39fe517fed6ba3f3a83668
ChaimaaAsri <108627246+Chaimaa-as@users.noreply.github.com>
2024-01-22 16:33:20 +0100

    imgUrl dynamique

--- front/js/script.js
+++ front/js/script.js
@@ -2,15 +2,16 @@ const url = "http://localhost:3000/api/products";
 
 fetch(url)
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    const imageUrl = data[0].imageUrl;
+    console.log("url de l'image: ", imageUrl);
 
-const anchor = document.createElement("a");
-anchor.href = "http://localhost:3000/images/kanap01.jpeg";
-anchor.text = "super kanap!";
+    const anchor = document.createElement("a");
+    anchor.href = imageUrl;
+    anchor.text = "super kanap!";
 
-const items = document.querySelector("#items");
-// items.appendChild(anchor);
-// ou
-if (items !== null) {
-  items.appendChild(anchor);
-}
+    const items = document.querySelector("#items");
+    if (items !== null) {
+      items.appendChild(anchor);
+    }
+  });
